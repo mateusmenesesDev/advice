1806674e338e65c5d9f8330b7ee0914ebab21e7b
Mateus Meneses <mateusppa10@gmail.com>
2022-10-23 13:44:36 -0300

    Adicionando try/catch a requisição

--- src/api/advice.ts
+++ src/api/advice.ts
@@ -1,7 +1,12 @@
 export const adviceApi = {
     getAdvice: async ()=>{
-        const http = await fetch('https://api.adviceslip.com/advice', {cache: 'no-store'})
-        const response = await http.json()
-        return response.slip
+        try {
+            const http = await fetch('https://api.adviceslip.com/advice', {cache: 'no-store'})
+            const response = await http.json()
+            return response.slip
+        }
+        catch{
+            alert('ouve algum problema. Tente novamente mais tarde!')
+        }
     }
 }
